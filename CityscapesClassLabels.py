042387e49dbb610077f213446a360969452dc52b
Cloudwalker <pranav13phoenix@gmail.com>
2022-05-27 05:13:00 +0530

    Update CityscapesClassLabels.py

--- CityscapesClassLabels.py
+++ CityscapesClassLabels.py
@@ -33,7 +33,7 @@ from einops.layers.torch import Rearrange
 # helpers
 from einops import reduce
 
-!wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=pranavphoenix&password=Newton@13&submit=Login' https://www.cityscapes-dataset.com/login/
+!wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username=USERNAME&password=PASSWORD&submit=Login' https://www.cityscapes-dataset.com/login/
 !wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
 !wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
 
